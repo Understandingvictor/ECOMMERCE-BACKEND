b4b45c0903ffa8c057f99ad0ee4b352b8a14387c
ifeanyi victor <victorflawless7@gmail.com>
2025-08-17 20:45:33 +0100

    modified get all users endpoint

--- routes/users.route.js
+++ routes/users.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import isAdmin from "../middlewares/isAdmin.middleware.js";
+import verifyUser from "../middlewares/userAuth.middleware.js";
 import { createUser, getAllUsers } from "../controllers/users.controller.js";
 
 const route = express.Router();
 route.post('/createUser', createUser);
-route.get("/getAllUsers/:userId", isAdmin, getAllUsers);
+route.get("/getAllUsers/", verifyUser, isAdmin, getAllUsers);
 
 export default route;
